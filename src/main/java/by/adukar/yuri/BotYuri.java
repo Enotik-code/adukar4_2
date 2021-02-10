f7bde320be7b41983520ed2e3e22eb804ea5c34b
Nesmejana <yuravoin8@bk.ru>
2021-02-10 13:41:45 +0300

    Юра

--- src/main/java/by/adukar/yuri/BotYuri.java
+++ src/main/java/by/adukar/yuri/BotYuri.java
@@ -1,9 +1,16 @@
 package by.adukar.yuri;
 
+import by.adukar.danil.telegrambot.time.TimeService;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Document;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BotYuri extends TelegramLongPollingBot {
     String word;
@@ -15,38 +22,69 @@ public class BotYuri extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         String message = update.getMessage().getText();
 
-        /*if(message.equals("/toUp"+word)) {
-            word = word.substring(7, word.length());
-            sendMsg(word.toUpperCase(), update.getMessage().getChatId());
+        if(message.equals("/replace")){
+            StringBuffer strBuffer = new StringBuffer("hello world!");
+            String str1 = strBuffer.substring(6,strBuffer.length());
+            System.out.println(str1);
+            sendMsg(str1,update.getMessage().getChatId());
+            System.out.print("Возвращаемое значение: ");
+            System.out.println(str1.replace('o','a'));
+            String str2 = str1.replace('o','a');
+            sendMsg("Возвращаемое значение: " + str2, update.getMessage().getChatId());
+
+
         }
-        if(message.equals("/toLow"+word)) {
-            word = word.substring(8, word.length());
-            sendMsg(word.toLowerCase(), update.getMessage().getChatId());
+
+
+
+
+
+
+        /*StringBuffer strBuffer2 = new StringBuffer("hello java!");
+        String str4 = strBuffer1.substring(6); // обрезка строки с 6 символа до конца
+        System.out.println(str4); //java!
+
+        String str2 = strBuffer1.substring(3, 9); // обрезка строки с 3 по 9 символ
+        System.out.println(str2); //lo jav
+
+        String Str = new String("Добро пожаловать на ProgLang.su");
+
+        System.out.print("Возвращаемое значение: " );
+        System.out.println(Str.replace('s', 'b'));
+
+        System.out.print("Возвращаемое значение: " );
+        System.out.println(Str.replace('о', 'а'));
+
+
+
+
+
+
+
+
+        if(message.equals("/replace")){
+            word = word.substring(9, message.length());
+            sendMsg(word.replace('o', 'a'), update.getMessage().getChatId());
+            System.out.println(word);
+            sendMsg("Здравствуйте, " + update.getMessage().getFrom().getFirstName() + "!", update.getMessage().getChatId());
         }
 
          */
-
-       if(message.equals("/replace")){
-           word = word.substring(9, word.length());
-           sendMsg(word.replace("а", "о"), update.getMessage().getChatId());
-           System.out.println(word);
-           sendMsg("Здравствуйте, " + update.getMessage().getFrom().getFirstName() + "!", update.getMessage().getChatId());
-       }
-
-
-
-
 
         if(message.equals("/start")){
             sendMsg("Здравствуйте, " + update.getMessage().getFrom().getFirstName() + "!", update.getMessage().getChatId());
         }
         if(message.equals("/help")){
-            sendMsg("Команды для работы с ботом: /start - начало чата, /help - список команд", update.getMessage().getChatId());
+            sendMsg("Команды для работы с ботом: /start - начало чата, /help - список команд /replace", update.getMessage().getChatId());
         }
 
 
 
+
+
     }
+
+
 
 
 
